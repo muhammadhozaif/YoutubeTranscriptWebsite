41d01a62c4598b2d676646a013573e0f1c8424d6
hozaif <04072212010@student.qau.edu.pk>
2025-09-12 23:31:58 +0500

    fixed transcript fetching logic

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,14 @@ from pydantic import BaseModel
 from youtube_transcript_api import YouTubeTranscriptApi
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 app = FastAPI()
-origins = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
+origins = os.getenv("FRONTEND_URL", "http://localhost:5173")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],  
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -30,8 +31,15 @@ def get_video_id(url: str) -> str:
 async def transcript(req: UrlRequest):
     try:
         video_id = get_video_id(req.url)
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        text = "\n".join([entry["text"] for entry in transcript])
+
+        yt = YouTubeTranscriptApi()
+
+        fetched = yt.fetch(video_id, languages=["en"])
+        raw = fetched.to_raw_data() 
+
+        text = "\n".join([entry["text"] for entry in raw])
+
         return {"transcript": text}
+
     except Exception as e:
         return {"error": str(e)}
